7ffad1fec75f165e002c3bf879b85d2253d99bed
Ben Arens <smartdevelopment44@gmail.com>
2025-06-24 18:31:31 +0200

    quantity_price

--- scripts/db.js
+++ scripts/db.js
@@ -5,37 +5,32 @@ let gerichte = [
         "name": "Pizza Krabben ",
         "description": "Mit Krabben und Peperoni",
         "price": 1.90,
-        "quantity_price": 0,
         "added": false
     }, 
     {
         "name": "Spagetti aglio e olio", 
         "description": "mit frischem Knoblauch", 
         "price": 9.99,
-        "quantity_price": 0,
         "added": false
     }, 
     {
         "name": "TK Dr. Ötker",
         "description": "Eine leckere TK",
         "price": 2.50,
-        "quantity_price": 0,
         "added": false
     },
     {
         "name": "Burger Basis (Vegetarisch)",
         "description": "Burger mit Erbsen-Patty",
         "price": 1.00,
-        "quantity_price": 0,
+        "added": false
+    },
+    {
+        "name": "Pumbas Döschen",
+        "description": "Lecker Döschen",
+        "price": 50.99,
         "added": false
     }
-    // {
-    //     "name": "Pumbas Döschen",
-    //     "description": "Lecker Döschen",
-    //     "price": 50.99,
-    //     "quantity_price": 0, 
-    //     "added": false
-    // }
 ];
 
 let cart = [
