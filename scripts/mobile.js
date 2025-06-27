9ab5f4676107f0dfd5c95cb9905102d8633e6c51
Ben Arens <smartdevelopment44@gmail.com>
2025-06-27 15:14:45 +0200

    template.js "geordnet"

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,12 +1,17 @@
 function toggleMobileCart(){
+ 
    let cartAbsoluteRef = document.getElementById('cart-absolute'); 
    cartAbsoluteRef.classList.toggle('hidden-mobile');
-
    let cartHeading = document.getElementById('cart-heading');
-   
+   fixMobileCart()
    if (cartHeading.innerHTML === "Warenkorb") {
     cartHeading.innerHTML = "schließen";
    } else {
     cartHeading.innerHTML = "Warenkorb";
    }
+}
+
+function fixMobileCart(){
+   let body = document.getElementById(`body`);
+   body.classList.toggle('fixoverflow-y');
 }
--- scripts/template.js
+++ scripts/template.js
@@ -14,13 +14,16 @@ function getGerichteTemplate(i){
 
 function getCartTemplate(i){
   return `
-         <div class="myOrder"><span class="gericht-cart">${cart[i].name}</span></div>
-         <div class="myOrder-quantity"> <i onclick="minusQuantity(${i})" class='icon-quantity bx  bx-minus'></i>  ${cart[i].quantity}x  <i onclick="plusQuantity(${i})" class='icon-quantity bx  bx-plus'></i> 
-         <div class="price">${cart[i].quantity_price}€</div>  <i onclick="removeItem(${i})" class='bx  bx-trash icon-trash'></i>   </div>
-         `
+    <div class="myOrder"><span class="gericht-cart">${cart[i].name}</span></div>
+    <div class="myOrder-quantity"> <i onclick="minusQuantity(${i})" class='icon-quantity bx  bx-minus'></i>  ${cart[i].quantity}x  <i onclick="plusQuantity(${i})" class='icon-quantity bx  bx-plus'></i> 
+    <div class="price">${cart[i].quantity_price}€</div>  <i onclick="removeItem(${i})" class='bx  bx-trash icon-trash'></i></div>
+    `
 }
 
 function getTotalTemplate(){
-    return `<div class="divider-cart"></div> <div class="total-price" >Summe: <span>${checkout[0].total.toFixed(2)}€</span><br>Versand: 5€ <br> <span class="total"> Gesamt: ${checkout[0].totalShipment.toFixed(2)}€</span> </div>
-    <a href="#" class="total-button" onclick="orderCompleted()" >Jetzt bestellen </a>`
+    return `
+    <div class="divider-cart"></div> 
+    <div class="total-price" >Summe: <span>${checkout[0].total.toFixed(2)}€</span><br>Versand: 5€ <br> <span class="total"> Gesamt: ${checkout[0].totalShipment.toFixed(2)}€</span> </div>
+    <a href="#" class="total-button" onclick="orderCompleted()" >Jetzt bestellen </a>
+    `
 }
